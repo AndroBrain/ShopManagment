b355ef4697d3cb270959dfc613ff8ace0cf148c4
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2022-11-11 15:43:42 +0100

    Add sending Email

---
+++ ShopManagmentAPI/domain/email/EmailSender.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagmentAPI.domain.service.email
+{
+    public class EmailSender : IEmailSender
+    {
+        public void sendEmail(string email, string title, string message)
+        {
+            // TODO
+        }
+    }
+}
---
+++ ShopManagmentAPI/domain/email/IEmailSender.cs
@@ -0,0 +1,6 @@
+﻿namespace ShopManagmentAPI.domain.service.email;
+
+public interface IEmailSender
+{
+    public void sendEmail(string email, string title, string message);
+}
